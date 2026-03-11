3532a2dd67d5358938a8fb097133fe10cc690123
Jared Samayoa <jsamayoapenagos@byupathway.edu>
2026-03-11 00:02:38 -0600

    W01 Project: Contacts Part 1: Added display message for index.

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const router = require("express").Router()
 
-// router.get("/", (req, res) => {res.send("INDEX")})
+router.get("/", (req, res) => {res.send("INDEX")})
 
 router.use("/users", require("./users"))
 
