0c05d3c5b053f733bfab5cb552d1c5f04a9c02de
Jared Samayoa <jsamayoapenagos@byupathway.edu>
2026-03-10 23:55:00 -0600

    W01 Project: Contacts Part 1

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 const router = require("express").Router()
 
-router.get("/", (req, res) => {res.send("INDEX")})
+// router.get("/", (req, res) => {res.send("INDEX")})
+
+router.use("/users", require("./users"))
 
 module.exports = router
--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require("express")
+const mongodb = require("./data/database")
 const app = express()
 
 const port = process.env.port || 3080
 
 app.use("/", require("./routes"))
 
-
-app.listen(port, ()=> {console.log(`Running on port ${port}`)})
+mongodb.initDb((err) => {
+    if (err){
+        console.log(err)
+    }
+    else{
+        app.listen(port, ()=> {console.log(`Database listening and Node running on port ${port}`)})
+    }
+})
